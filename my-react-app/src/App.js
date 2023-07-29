947840c9450ef4ddb1dedbae8655753990bf05a2
MohamadFawas <mohamadfawas19971003@gmail.com>
2023-07-29 15:37:18 +0530

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Nabar from './layout/Nabar';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AddUser from './users/AddUser';
+import ViewUser from './users/ViewUser';
+import Delete from './users/Delete';
+// import ViewUser from './users/ViewUser';
+function App() {
+  return (
+    <div>
+      <Router>
+
+      <Nabar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/addUser" element={<AddUser/>}/>
+        <Route exact path="/viewUser/:id" element={<ViewUser/>}/>
+        <Route exact path="/delete/:id" element={<Delete/>}/>
+      </Routes>
+      
+
+      </Router>
+      
+    </div>
+  );
+}
+
+
+export default App;
